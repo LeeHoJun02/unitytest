bbb7d95c67201277ffbac6e1a91ad496daa82109
LeeHoJun02 <andrew5658@naver.com>
2025-09-08 19:56:30 +0900

    점프 구현
    이동 및 점프 로직 구현 중

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    private float currentSpeed;
+    private float sprintSpeed = 10.0f;
     private float speed = 5.0f;
+    private float jumpForce = 5.0f;
     private float moveInput;
+
+    private bool isGrounded;
 
     private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,7 +20,35 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    void FixedUpdate()
+    {
         moveInput = Input.GetAxisRaw("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(moveInput * currentSpeed, rb.linearVelocity.y);
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = sprintSpeed;
+        }
+        else
+        {
+            currentSpeed = speed;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collsion)
+    {
+        if(collsion.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
