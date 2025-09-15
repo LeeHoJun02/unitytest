763e657c5a9789563f3892ac4d1d2bed800e31a0
LeeHoJun02 <andrew5658@naver.com>
2025-09-15 22:46:14 +0900

    애니메이션 수정 필요
    애니메이션 작동 트리거 이상으로 수정 필요

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,28 +1,57 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerControl : MonoBehaviour
 {
+    enum PlayerState
+    {
+        Idle,
+        Walking,
+        Running,
+        Jumping,
+        Falling,
+        Attacking,
+        UsingAbility1,
+        UsingAbility2
+    }
+
+    private float currentHealth = 100.0f;
+    private float maxHealth = 100.0f;
+    private float currentStamina = 50.0f;
+    private float maxStamina = 50.0f;
+
     private float currentSpeed;
     private float sprintSpeed = 10.0f;
     private float speed = 5.0f;
     private float jumpForce = 7.0f;
     private float moveInput;
 
+    private PlayerState currentState;
+
     private bool isGrounded;
     private bool isJumping;
     private bool isSprinting;
+    private bool isAttacking;
+
+    private Coroutine staminaRoutine;
 
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = rb.GetComponent<SpriteRenderer>();
+        anim = rb.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandleInput();
+        MainHandleInput();
+        HandlePlayerState();
     }
 
     void FixedUpdate()
@@ -32,7 +61,7 @@ public class PlayerControl : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collsion)
     {
-        if(collsion.gameObject.CompareTag("Ground"))
+        if (collsion.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
         }
@@ -40,22 +69,109 @@ public class PlayerControl : MonoBehaviour
 
     void OnCollisionExit2D(Collision2D collsion)
     {
-        if(collsion.gameObject.CompareTag("Ground"))
+        if (collsion.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
         }
     }
 
-    void HandleInput()
+    void HandlePlayerState()
     {
+        // 요 부분은 gpt가 작성한 부분이여서 나중에 추가적으로 더 이해 및 생각
+        if (!isGrounded)
+        {
+            float vy = rb.linearVelocity.y;
+            if (vy > 0.1f)
+            {
+                currentState = PlayerState.Jumping;
+                Debug.Log("Jumping");
+            }
+            else if (vy < -0.1f)
+            {
+                currentState = PlayerState.Falling;
+                Debug.Log("Falling");
+            }
+            else currentState = PlayerState.Jumping;
+        }
+        else
+        {
+            if (isSprinting && Mathf.Abs(moveInput) > 0.01f && currentStamina > 0)
+            {
+                currentState = PlayerState.Running;
+                Debug.Log("Running");
+                anim.SetBool("Run", true);
+            }
+            else if (Mathf.Abs(moveInput) > 0.01f)
+            {
+                currentState = PlayerState.Walking;
+                Debug.Log("Walking");
+                anim.SetBool("move", true);
+            }
+
+            else
+            {
+                currentState = PlayerState.Idle;
+                Debug.Log("Idle");
+                anim.SetBool("Stop", true);
+
+            }
+
+        }
+        //요기까지
+        if (isAttacking)
+        {
+            Debug.Log("Attacking");
+        }
+    }
+
+    //여기서부터 다시 공부해서 하기 이건 도저히 아직 이해가 안된다
+    IEnumerator regenStamina()
+    {
+        yield return new WaitForSeconds(2);
+        while (currentStamina < maxStamina)
+        {
+            currentStamina += 10 * Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    void MainHandleInput()
+    {
+        HandlMoveInput();
+        HandleAttackInput();
+    }
+
+    void HandlMoveInput()
+    {
+
         moveInput = Input.GetAxisRaw("Horizontal");
+        if (moveInput < 0)
+        {
+            sprite.flipX = true;
+        }
+        else if (moveInput > 0)
+        {
+            sprite.flipX = false;
+        }
         isSprinting = Input.GetKey(KeyCode.LeftShift);
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             isJumping = true;
         }
     }
 
+    void HandleAttackInput()
+    {
+        isAttacking = Input.GetMouseButtonDown(0);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Use Ability2");
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("Use Ability1");
+        }
+    }
     void ApplyMovement()
     {
         rb.linearVelocity = new Vector2(moveInput * currentSpeed, rb.linearVelocity.y);
@@ -64,6 +180,22 @@ public class PlayerControl : MonoBehaviour
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isJumping = false;
         }
-        currentSpeed = (isSprinting) ? sprintSpeed : speed;
+        if (isSprinting && currentStamina > 0 && currentState == PlayerState.Running)
+        {
+            currentSpeed = sprintSpeed;
+            currentStamina -= 5 * Time.deltaTime;
+        }
+        else
+        {
+            currentSpeed = speed;
+        }
+    }
+
+
+    //gpt
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 100, 20), "Health: " + (int)currentHealth + "/" + (int)maxHealth);
+        GUI.Box(new Rect(10, 40, 100, 20), "Stamina: " + (int)currentStamina + "/" + (int)maxStamina);
     }
 }
